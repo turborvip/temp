c0b335aa1b0171cd737640bfc2647228d2bc5c01
turborvip <86955194+turborvip@users.noreply.github.com>
2022-08-19 00:05:32 +0700

    update view

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "/static")));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    // res.sendfile("src/views/home.html");
+    res.render("home");
 });
   
 const server = app.listen(process.env.PORT, () => {
