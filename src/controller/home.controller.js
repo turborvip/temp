daa4982cd89c1c273f983282da2d9f814d7c7c73
turborvip <86955194+turborvip@users.noreply.github.com>
2022-08-19 16:00:03 +0700

    ao that day

---
+++ src/controller/home.controller.js
@@ -0,0 +1,7 @@
+const homePage = (req,res) => {
+    return res.render('../views/home.ejs')
+}
+
+module.exports = {
+    homePage
+}
--- src/routers/router.js
+++ src/routers/router.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controller/user.controller') 
+const homeController = require('../controller/home.controller')
 
 router.get("/getUser", userController.getUser);
 
 router.get("/createUser", userController.createUser);
+
+router.get("/home", homeController.homePage);
 
 module.exports = router
